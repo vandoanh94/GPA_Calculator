51b4acf4772f77edb475709d3976d3992db65b33
ORIENTSOFTWARE\nvdoanh <vandoanh94@gmail.com>
2017-10-17 14:58:15 +0700

    xml to data array done

--- my-app/src/app/xml-convert/xml-convert.component.ts
+++ my-app/src/app/xml-convert/xml-convert.component.ts
@@ -1,32 +1,30 @@
 import { Component } from '@angular/core';
 import * as xml2js from "xml2js";
-import { XmlModel } from "./xml-convert.model";
+import { Course } from '../gpa-calculator/Course';
 
 @Component({
     selector: 'xml-convert',
     templateUrl: './xml-convert.component.html',
     styleUrls: ['./xml-convert.component.css']
 })
+
 export class xmlConvertComponent {
     xml: any;
     title = 'XML CONVERT';
-    xmlconvert: string;
-    xmlModel : XmlModel[]=[];
+    xmlconvert:string = "";
+    courses : Course[] =[];
     convert(any) {
+        let obj4 : string = "";
+        let data : Course[] =[];
         xml2js.parseString(any, function (err, result) {
-            console.log("xml", result);
-            console.log("model",result.Report.Table1["0"]);
-            //this.xmlModel = JSON.parse(result.Report.Table1).json() as XmlModel[];
-            //console.log("json",this.xmlModel);
-            // result.Report.Table1.forEach(res => {
-                
-            // });
-            //this.xmlconvert = result.Report.Table1["0"].TXTTENMON["0"];
+            let table = result.Report.Table1;
+            table.forEach(element => {
+                obj4 += element.TXTTENMON[0];
+                data.push({Day: element.TEXT33[0],CourseID: element.TEXT34[0],CourseTitle: element.TXTTENMON[0],Grade: element.TEXT36[0],CreditEarned: element.TEXT4[0]});
+            });
             
-            console.log("convert",result.Report.Table1["0"].TXTTENMON["0"]);
-            Object.assign(this.XmlModel,result.Report.Table1);
-            console.log("json",JSON.stringify(result.Report.Table1));
         });
-
+        this.xmlconvert = obj4;
+        this.courses = data;
     }
 }
--- my-app/src/app/xml-convert/xml-convert.model.ts
+++
@@ -1,9 +0,0 @@
-export class XmlModel {
-    TEXT2: any[];
-    TEXT4: any[];
-    TEXT7: any[];
-    TEXT33: any[];
-    TEXT34: any[];
-    TEXT36: any[];
-    TXTTENMON: any[];
-}
